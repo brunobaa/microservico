0987b129b6a5dc8fc090f38467c2471011845032
paphartmann <pedroaphartmann@gmail.com>
2025-08-27 21:47:58 -0300

    init files

---
+++ currency-exchange-service/src/main/java/com/engsoft2/currency_exchange_service/ICurrencyExchangeRepository.java
@@ -0,0 +1,5 @@
+import org.springframework.data.repository.CrudRepository;
+public interface CurrencyExchangeRepository extends
+CrudRepository<CurrencyExchange,Long> {
+CurrencyExchange findByFromAndTo(String from, String to);
+}
